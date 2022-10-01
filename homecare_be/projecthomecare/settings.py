3ccf2d9b74e09737c692b686af7d984349f95031
Leonardo Gomez <leonardo.gomez.es@gmail.com>
2022-10-01 17:11:50 -0500

    primera prueba del servidor ok

--- homecare_be/projecthomecare/settings.py
+++ homecare_be/projecthomecare/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
-    'homecareApp'
+    'rest_framework',
+    'homecareApp',
 ]
 
 MIDDLEWARE = [
